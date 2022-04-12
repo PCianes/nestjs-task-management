449a1a2cbe3b147b51ec67235c53f8aa4a90fdc6
Pablo Cianes <cianespablo@gmail.com>
2022-04-12 14:24:38 +0200

    🚩 add task ownership & restrictions by user

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -3,13 +3,17 @@ import { Task } from './task.entity';
 import { TasksStatus } from './task-status.enum';
 import { createTaskDto } from './dto/create-task-dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
+import { User } from 'src/auth/user.entity';
 
 //TODO: update typeorm -> https://github.com/nestjs/typeorm/pull/1233
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
+
+    //* only user's tasks
+    query.where({ user });
 
     if (status) {
       query.andWhere('task.status = :data', { data: status });
@@ -17,7 +21,9 @@ export class TasksRepository extends Repository<Task> {
 
     if (search) {
       query.andWhere(
-        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        //!all this query inside () to avoid problems in the final result of all "wheres"
+        // because of the OR as final statment can return other user's tasks that match with search
+        '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
         { search: `%${search}%` },
       );
     }
@@ -27,12 +33,13 @@ export class TasksRepository extends Repository<Task> {
     return tasks;
   }
 
-  async createTask(createTaskDto: createTaskDto): Promise<Task> {
+  async createTask(createTaskDto: createTaskDto, user: User): Promise<Task> {
     const { title, description } = createTaskDto;
     const task = this.create({
       title,
       description,
       status: TasksStatus.OPEN,
+      user,
     });
 
     await this.save(task);
